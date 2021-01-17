1242d063c6fa7d1ea14aa34e3efdb4d2aba7ff9f
Alexander Permyakov <pamugk@gmail.com>
2021-01-17 19:51:00 +0500

    Settings dialog

--- src/main/kotlin/ru/psu/web-ontology-doc-checker/app.kt
+++ src/main/kotlin/ru/psu/web-ontology-doc-checker/app.kt
@@ -4,23 +4,40 @@ import com.ccfraser.muirwik.components.*
 import com.ccfraser.muirwik.components.button.mButton
 import com.ccfraser.muirwik.components.button.mIconButton
 import com.ccfraser.muirwik.components.card.mCard
-import kotlinx.css.FlexBasis
+import com.ccfraser.muirwik.components.dialog.*
+import com.ccfraser.muirwik.components.form.MFormControlVariant
 import kotlinx.css.LinearDimension
 import kotlinx.css.flex
 import kotlinx.css.marginLeft
-import react.RBuilder
-import react.RComponent
-import react.RProps
-import react.RState
+import kotlinx.html.InputType
+import react.*
 import styled.css
 
-class App(props: RProps) : RComponent<RProps, RState>(props) {
+class AppState(
+    var K: Int,
+    var N: Int,
+    var showDialog: Boolean
+): RState
+
+class App(props: RProps) : RComponent<RProps, AppState>(props) {
+
+    init {
+        state = AppState(
+            K = 1,
+            N = 10,
+            showDialog = false
+        )
+    }
 
     override fun RBuilder.render() {
+        fun handleCloseDialog() {
+            setState { showDialog = false}
+        }
+
         mAppBar(position = MAppBarPosition.sticky) {
             mToolbar {
                 mTypography("Онтологический упорядочиватель документов")
-                mIconButton("settings") {
+                mIconButton("settings", onClick = { setState { showDialog = true } }) {
                     css {
                         marginLeft = LinearDimension.auto
                     }
@@ -30,6 +47,32 @@ class App(props: RProps) : RComponent<RProps, RState>(props) {
         mCard {
             css {
                 flex(1.0, 1.0)
+            }
+        }
+        mDialog(state.showDialog, onClose =  { _, _ -> handleCloseDialog() }) {
+            mDialogTitle("Настройки")
+            mDialogContent {
+                mDialogContentText("""
+                    Основные настройки:
+                    N - семантическое расстояние между вершинами онтологии;
+                    K - количество предложений между понятиями в исходном тексте.
+                """.trimIndent())
+                mTextField("N",
+                    onChange = {
+                        val value = it.targetInputValue.toIntOrNull() ?: state.N
+                        if (value > 0 && value < 99) setState { N = value }
+                    },
+                    type = InputType.number,
+                    value = state.N.toString(),
+                    variant = MFormControlVariant.outlined)
+                mTextField("K",
+                    onChange = {
+                        val value = it.targetInputValue.toIntOrNull() ?: state.K
+                        if (value > 0 && value < 99) setState { K = value }
+                    },
+                    type = InputType.number,
+                    value = state.K.toString(),
+                    variant = MFormControlVariant.outlined)
             }
         }
     }
