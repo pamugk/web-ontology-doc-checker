d465a85e402698fab70c8c8c4ec62a574f2e841d
Alexander Permyakov <pamugk@gmail.com>
2021-01-17 13:58:17 +0500

    added material icons stylesheet; basic appbar

--- src/main/kotlin/ru/psu/web-ontology-doc-checker/app.kt
+++ src/main/kotlin/ru/psu/web-ontology-doc-checker/app.kt
@@ -1,22 +1,36 @@
 package ru.psu.web_ontology_doc_checker
 
+import com.ccfraser.muirwik.components.*
+import com.ccfraser.muirwik.components.button.mButton
+import com.ccfraser.muirwik.components.button.mIconButton
 import com.ccfraser.muirwik.components.card.mCard
-import com.ccfraser.muirwik.components.mAppBar
-import com.ccfraser.muirwik.components.mToolbar
-import com.ccfraser.muirwik.components.mTypography
+import kotlinx.css.FlexBasis
+import kotlinx.css.LinearDimension
+import kotlinx.css.flex
+import kotlinx.css.marginLeft
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
+import styled.css
 
 class App(props: RProps) : RComponent<RProps, RState>(props) {
 
     override fun RBuilder.render() {
-        mAppBar {
+        mAppBar(position = MAppBarPosition.sticky) {
             mToolbar {
                 mTypography("Онтологический упорядочиватель документов")
+                mIconButton("settings") {
+                    css {
+                        marginLeft = LinearDimension.auto
+                    }
+                }
             }
         }
-        mCard { }
+        mCard {
+            css {
+                flex(1.0, 1.0)
+            }
+        }
     }
 }
